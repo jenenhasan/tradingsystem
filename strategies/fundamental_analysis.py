15fc022fc437e2c11d5c7f8eaba71b7bf30ec6d0
jenen hasan <jenenhasan77@gmail.com>
2024-12-02 15:11:27 +0200

    add the fundamental_analysis logic to the trading_bot , but note it is slow

--- strategies/fundamental_analysis.py
+++ strategies/fundamental_analysis.py
@@ -1,13 +1,14 @@
 import yfinance as yf
 import pandas as pd 
 from datetime import datetime
-
+import os 
 
 
 class data_fetcher : 
     def __init__(self, tickers=None, file_path=None):
         self.tickers = tickers
-        self.file_path = 'C:/Users/jenen/Desktop/Atomatedtradingbot/sp500_financail_data.csv'
+        self.file_path = file_path
+
         self.data = []
 
     # Fetch the S&P 500 list
@@ -17,12 +18,13 @@ class data_fetcher :
         df = tables[0]  # Just access the first column because it has the names 
         tickers = df['Symbol'].tolist()  # Change the first column, 'Symbol', to a list 
         return tickers
+    
 
     #later i can enhanc it using web scraping 
     # Fetch data from the API
     def fetch_financial_data(self , ticker):
         stock = yf.Ticker(ticker)
-        info = stock.inf
+        info = stock.info
         return {
         "ticker": ticker,
         "pe_ratio": info.get("trailingPE"),
@@ -30,6 +32,8 @@ class data_fetcher :
         "roe": info.get("returnOnEquity"),
         "debt_to_equity": info.get("debtToEquity"),
         "dividend_yield": info.get("dividendYield"),
+        'industry' : info.get('industry'),
+        'sector' : info.get('sector')
         }
 
     # Loop through the S&P 500 tickers and fetch data
@@ -44,25 +48,36 @@ class data_fetcher :
     # Save the fetched data to CSV
     def save_sp500_data_to_csv(self , tickers):
         sp500_data = self.fetch_sp500_fdata(tickers)
+        if not sp500_data:  # Check if data is empty
+            raise ValueError("No data fetched for the provided tickers.")
         df = pd.DataFrame(sp500_data)
-        df.to_csv('sp500_financail_data.csv', index=False)
-        print('Saved sp500 financial data to sp500_financial_data.csv')
-
-
-    # Load the S&P 500 financial data from the CSV file
-    def load_sp500_data_from_csv(self ):
-        df = pd.read_csv(self.file_path)
+        df.to_csv(self.file_path, index=False)
+        print(f"Saved S&P 500 financial data to {self.file_path}")
         return df
 
+
 class fundamental_analyzer : 
-    def __init__(self, data):
+    def __init__(self, data , fill_missing=True):
         self.data = data 
+        self.fill_missing = fill_missing
+    def handle_missing_data(self):
+        if self.fill_missing : 
+            for i in ['pe_ratio', 'revenue_growth', 'roe', 'debt_to_equity', 'dividend_yield'] : 
+                if i in self.data.columns : 
+                    self.data[i] = pd.to_numeric(self.data[i] , errors= 'coerce')
+                    self.data[i] = self.data.groupby('industry')[i].transform(lambda x : x.fillna(x.median()))
+                else : 
+                    self.data = self.data.dropna(subset =['pe_ratio', 'roe', 'debt_to_equity', 'dividend_yield'] )
+                
+
+
      
 
     
     # Apply fundamental analysis filter
     def perform_fundamental_analysis(self):
         df = self.data
+        self.handle_missing_data()
         # Convert relevant columns to numeric, invalid values to NaN
         df['pe_ratio'] = pd.to_numeric(df['pe_ratio'], errors='coerce')
         df['revenue_growth'] = pd.to_numeric(df['revenue_growth'], errors='coerce')
@@ -75,10 +90,10 @@ class fundamental_analyzer :
 
         #it still need enhancement about the comparison stratigie 
         #calculate the dynamic values from the database
-        pe_ratio_threshold = df['pe_ratio'].quantile(0.25)  # how much investors are willing to pay for each dollar of earnings.
-        debt_to_equity_threshold = df['debt_to_equity'].quantile(0.25)  # total debt to its equity
-        dividend_yield_threshold = df['dividend_yield'].quantile(0.75)  #how much income an investor can expect to receive from dividends as a percentage of the stock price
-        roe_threshold = 0.15 #how effectively a company uses shareholder equity to generate profits
+        pe_ratio_threshold = df['pe_ratio'].quantile(0.5)  # how much investors are willing to pay for each dollar of earnings.
+        debt_to_equity_threshold = df['debt_to_equity'].quantile(0.5)  # total debt to its equity
+        dividend_yield_threshold = df['dividend_yield'].quantile(0.5)  #how much income an investor can expect to receive from dividends as a percentage of the stock price
+        roe_threshold = 0.10 #how effectively a company uses shareholder equity to generate profits
         revenue_growth_threshold = 0    # shows the company’s ability to increase sales over time
 
         #the filter is strict (chose it i want to focus on quality over quantity )
@@ -90,32 +105,49 @@ class fundamental_analyzer :
             (df['debt_to_equity'] <= debt_to_equity_threshold) & # Lower debt-to-equity ratio
             (df['dividend_yield'] >= dividend_yield_threshold)  # Higher dividend yield
             ]
+        
+
+        if self.data is None or self.data.empty:
+            raise ValueError("The DataFrame is empty or not loaded.")
         return filtered_df
+        
+
 
 
 def main():
     
-    file_path = 'C:\\Users\\jenen\\Desktop\\Atomatedtradingbot\\sp500_financail_data.csv'
-    stock_fetcher = data_fetcher(file_path=file_path)
-    
-    # stock_fetcher.fetch_sp500_tickers()
-    # stock_fetcher.fetch_data()
-    # stock_fetcher.save_data_to_csv()
-    
-    # Load data from CSV
-    df = stock_fetcher.load_sp500_data_from_csv()
+    filtered_file_path = 'C:\\Users\\jenen\\Desktop\\Atomatedtradingbot\\filtered_sp500_financial_data_2024-11-29_22-12-26.csv'
+    sp500_file_path = 'C:\\Users\\jenen\\Desktop\\Atomatedtradingbot\\sp500_financail_data.csv'
 
+    fetcher =  data_fetcher(file_path=sp500_file_path)
+
+    tickers  = fetcher.fetch_sp500_ticker() 
+    sp500_df= fetcher.save_sp500_data_to_csv(tickers)
+    if sp500_df is None or sp500_df.empty:
+        raise ValueError("Failed to fetch or save S&P 500 financial data.")
+
+    
+    
     # Initialize the StockAnalyzer class
-    stock_analyzer = fundamental_analyzer(df )
+    stock_analyzer = fundamental_analyzer(sp500_df)
     
     # Perform fundamental analysis and get filtered data
     filtered_df = stock_analyzer.perform_fundamental_analysis()
 
     # Save filtered data to CSV with a timestamp
     
-    filtered_df.to_csv('C:\\Users\\jenen\\Desktop\\Atomatedtradingbot\\filtered_sp500_financial_data_2024-11-29_22-12-26.csv', index=False)
+    filtered_df.to_csv(filtered_file_path, index=False)
+    print(f'filtered data saved to {filtered_file_path}')
     print(filtered_df.head())
+    return filtered_df
+
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
 
--- trading.py
+++ trading.py
@@ -5,16 +5,18 @@ from lumibot.traders import Trader # for deployement capability
 from datetime import datetime    #we will need this for the backtesting
 from dotenv import load_dotenv
 import os 
+import pandas as pd 
 #from alpaca.trading.client import TradingClient
+
+
+from strategies.fundamental_analysis import main
+
 
 
 #from alpaca_trade_api import REST 
 #from timedelta import Timedelta 
 #from finbert_utils import estimate_sentiment
-
-
 load_dotenv()
-
 #variables needed
 API_KEY =os.getenv("API_KEY") 
 API_SECRET = os.getenv("API_SECRET")
@@ -31,47 +33,68 @@ ALPACA_CREDS = {
 
 }
 
-
-
-
-
-
 class trading_strategy (Strategy):
 
     
     #instance method (this will run once )
-    def initialize(self ,symbol:str = "SPY"): 
+    def initialize(self ,symbol:str = "SPY"  , cash_at_risk:float = .5): 
       
         self.symbol = symbol
         self.sleep_time = "24H"
         self.last_trade = None
-
-
-    #run every time we get new data
-    def on_trading_iteration(self):
-        if self.last_trade == None :
-            order = self.create_order(
-                self.symbol,
-                10, 
-                'buy',
-                type = 'market'
-            )
-            self.submit_order(order)
-            self.last_trade = 'buy '
-
-    #method to mangange the ammount of money we want to risk 
-    def postion_sizing (self) : 
-        # cash = self.get_cash()
-        # last_price_stock = self.get_last_price(self.symbol)
-        # risk_per_trade  = cash * riskperc
+        self.cash_at_risk = cash_at_risk 
+        self.filtered_df = pd.read_csv('filtered_sp500_financial_data_2024-11-29_22-12-26.csv')
         
-        # share_to_buy = round(risk_per_trade /last_price_stock , 0 )
-        # return cash , last_price_stock , share_to_buy
+
+        #method how many cash we want to place for each trade
+    def postion_sizing (self) : 
 
         cash = self.get_cash() 
         last_price = self.get_last_price(self.symbol)
-        quantity = round(cash * self.cash_at_risk / last_price,0)
+        quantity = round(cash * self.cash_at_risk / last_price,0) 
         return cash, last_price, quantity
+    
+    def stop_loss (self ):
+        stop_loss_amm = (self.last_price ) * (1 - self.cash_at_risk)
+        return stop_loss_amm
+    #it still need to be connected to the iternation 
+    def perform_fundamental_analysis(self):
+        try : 
+            self.filtered_df = main()
+            if self.filtered_df is None or self.filtered_df.empty : 
+                raise ValueError('filtered data is empty ')
+            print('fundamental analysis performed successfully')
+
+        except : 
+            print('error in fa')
+            
+        
+        
+#####################try to fix the overlooping 
+
+    #run every time we get new data
+    def on_trading_iteration(self):
+        cash, last_price, quantity  = self.postion_sizing()
+        # inorder not to buy when we dont have cash
+        if cash > last_price : 
+            if self.symbol in self.filtered_df['ticker'].values:
+                #if for the first trade 
+                if self.last_trade == None : #####fix this it has to check just once 
+                    order = self.create_order(
+                        self.symbol,
+                        quantity,
+                        10, 
+                        'buy',
+                        type = 'market'
+                        )
+                    self.submit_order(order)
+                    self.last_trade = 'buy '
+    
+
+    def buy ():
+        pass
+    def sell():
+        pass
 
 
     
@@ -84,18 +107,13 @@ class trading_strategy (Strategy):
     def fundemental_analysis():
         pass
 
-
-    
-
-
-  
-       
-
+ 
 backtesting_start_date = datetime(2022 ,11 , 15 )
-backtesting_end_date = datetime(2023,12 , 20)
+backtesting_end_date = datetime(2024,11 ,23)
 broker = Alpaca(ALPACA_CREDS)
 strategy= trading_strategy(name = 'mlstrat' , broker= broker , 
-                           parameters = {'symbol' : 'SPY'})
+                           parameters = {'symbol' : 'SPY' ,
+                                          'cash_at_risk' :.5  })
 
 
 #run the backtesting
@@ -103,9 +121,38 @@ strategy.backtest(
     YahooDataBacktesting , 
     backtesting_start_date , 
     backtesting_end_date , 
-    parameters = {'symbol' : 'SPY'}
+    parameters = {'symbol' : 'SPY', 
+                  'cash_at_risk' :.5 }   #higher cash at risk men more cash per trade 
 )
 
 
 
+# Yes, it is possible that the conditional statements in this section could be contributing to performance issues, especially if they are being executed repeatedly in each iteration. Let's break it down:
 
+# Repeated Condition Checks:
+
+# The code has multiple if checks inside the method. In particular:
+# Checking if cash > last_price
+# Checking if self.symbol in self.filtered_df['ticker'].values
+# Checking if self.last_trade == None These checks are being executed every time the method on_trading_iteration is called. If this method is being called frequently (e.g., in every iteration of a trading loop), these checks can accumulate and take significant time if there are many symbols or orders involved.
+# Dataframe Filtering (self.symbol in self.filtered_df['ticker'].values):
+
+# The line self.symbol in self.filtered_df['ticker'].values can be particularly slow if self.filtered_df['ticker'] contains a large number of rows. The in operation with a large DataFrame column is an O(n) operation, meaning it could be slow for large datasets.
+# If this check is happening often, it could cause the program to lag.
+# Order Creation (self.create_order()) and Submission (self.submit_order()):
+
+# Even though these functions may not directly cause performance issues, they can be part of the problem if they are tied to external processes like API calls or database operations. If these functions are performing network operations or heavy computations, it will add to the overall time spent in each iteration.
+# Suggestions to Improve Performance:
+# Optimize in Checks:
+
+# Instead of checking self.symbol in self.filtered_df['ticker'].values every iteration, consider pre-processing the filtered symbols into a set or list (which has O(1) lookup time) before the loop starts.
+# Refactor Conditionals:
+
+# Try to minimize the number of if checks in each iteration, especially if they are not necessary for every trade iteration. For example, if the condition self.last_trade == None only needs to be checked once per session, move that check outside of the main loop.
+# Profile Your Code:
+
+# Use Python's cProfile module or a similar profiler to determine which part of the code consumes the most time. This can help you pinpoint the bottleneck more effectively.
+# Asynchronous Calls:
+
+# If there are network calls or slow external processes, you could make the order submission process asynchronous to prevent blocking other operations.
+# By refactoring the logic and making these optimizations, you should be able to reduce the time taken per iteration and improve the performance of your trading algorithm.
